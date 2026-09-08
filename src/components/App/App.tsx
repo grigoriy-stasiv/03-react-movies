521c745222aef2ece2581471840c6cf05febbcda
your_nameGrigoriy <your_emailgrek436@gmail.com>
2026-09-08 12:12:55 +0300

    add_SearcBaar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import type { movie } from '../../types';
-import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
-import { Loader } from '../Loader/Loader';
-import { MoviGrid } from '../MovieGrid/MovieGrid';
-import { MovieModal } from '../MovieModal/MovieModal';
-import { SearchBar } from '../SearchBar/SearchBar';
+import type { Movie } from '../../types/movie';
+// import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
+// import { Loader } from '../Loader/Loader';
+// import { MoviGrid } from '../MovieGrid/MovieGrid';
+// import { MovieModal } from '../MovieModal/MovieModal';
+import  SearchBar  from '../SearchBar/SearchBar';
